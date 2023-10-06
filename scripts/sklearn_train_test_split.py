202ddec6eebafd02bba5087fcc31492a0e7dd6d4
jessedesimone <52834672+jessedesimone@users.noreply.github.com>
2023-10-06 10:39:47 -0400

    231006

--- scripts/sklearn_train_test_split.py
+++ scripts/sklearn_train_test_split.py
@@ -8,19 +8,36 @@ import os;
 import pandas as pd;
 import numpy as np;
 from sklearn.model_selection import train_test_split;
+import sys
 
 #define paths
 top = '/Users/jessedesimone/desimone_github/ml/aidp'
-#data_dir = os.path.join(top + '/infiles')
-data_dir = os.path.join(top + '/tests/resources')
+data_dir = os.path.join(top + '/infiles')
+#data_dir = os.path.join(top + '/tests/resources')
 os.chdir(data_dir)
 
+class FileError(OSError):
+    def __init__(self, filename):
+        self.filename = filename
+        
+    def __str__(self):
+        return f"++ Error in accessing file: {self.filename}\n ++ Check path"
+
 #read data
-df = pd.read_excel('1002_Data_no_Subj_Site.xlsx')
+infile='hello.xlsx'
+print('++ reading data')
+try:
+    df = pd.read_excel(infile)
+    print('++ File found')
+except Exception as e:
+    print (FileError(infile),e)
+    sys.exit()
 
 #train test split | save train and test data as outfiles
+print('++ Running train test split')
 train, test = train_test_split(df, test_size=0.2, random_state=42)
 print(train.shape)
 print(test.shape)
-train.to_excel('train_set.xlsx', index=False)
-test.to_excel('test_set.xlsx', index=False)
+train.to_excel('aidd_training.xlsx', index=False)
+test.to_excel('aidd_testing.xlsx', index=False)
+print('++ train/test split done')
