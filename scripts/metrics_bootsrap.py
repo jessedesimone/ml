9e42969413e16ac088d0fff21cef5904db75ac58
jessedesimone <52834672+jessedesimone@users.noreply.github.com>
2023-10-27 16:01:44 -0400

    231027

--- scripts/metrics_bootsrap.py
+++ scripts/metrics_bootsrap.py
@@ -14,6 +14,7 @@ import pandas as pd
 from sklearn.metrics import roc_auc_score, confusion_matrix
 import pickle
 import numpy as np
+import sys
 
 #version control
 # skvers = sklearn.__version__
@@ -32,8 +33,6 @@ class directories:
     infile_dir = os.path.join(proj_dir, 'resources/models/model_231027/')
     outfile_dir = os.path.join(top_dir, 'ml/mlreports/')
 
-#os.chdir(directories.script_dir)
-
 #create required directories
 dirs = [directories.log_dir, directories.outfile_dir]
 for dir in dirs: 
@@ -42,20 +41,26 @@ for dir in dirs:
         os.makedirs(dir)
 
 #configure logger
+os.chdir(directories.script_dir)
 from logger import configure_logger
 configure_logger(directories.log_dir)
 logger = logging.getLogger(__name__)
-logger.info("Starting code")
+logger.info("Starting bootstrap code")
 
 os.chdir(directories.infile_dir)
 logger.info("Infile directory: {}".format(directories.infile_dir))
-logger.info("Current kernel is", os.environ['CONDA_DEFAULT_ENV'])
+logger.info("Current kernel is " + os.environ['CONDA_DEFAULT_ENV'])
 
 #Load Dataframes
 logger.info("Loading dataframes")
 df_tr=pd.read_excel(directories.infile_dir +'aidd_training_model_231027.xlsx', header=0, index_col="Subject")
+logger.info("training model")
+logger.info(df_tr.head())
 df_te=pd.read_excel(directories.infile_dir +'aidd_testing_model_231027.xlsx', header=0, index_col="Subject")
+logger.info("testing model")
+logger.info(df_te.head())
 keep_columns=[
+    "GroupID",
     "dmri_ad_v_dlb (AD Probability)",
     "dmri_ad_v_con (AD Probability)",
     "dmri_ad_v_all (AD Probability)",
@@ -144,7 +149,6 @@ def bootstrapper(Y_true, Y_pred, savename: str, r_seed=42, n_bootstraps=1000):
                                "PPV": ppv_bs_scores, "NPV": npv_bs_scores, "AUC": auc})
     df_report.to_excel(directories.outfile_dir + savename + '_bootstrap_report.xlsx', index=0)
 
-
 def report_mean_ci(dfname: str):
     df = pd.read_excel(directories.outfile_dir + dfname + '_bootstrap_report.xlsx')
     df_sum=pd.DataFrame(index=df.columns, columns=["Mean", "Lower", "Upper"])
@@ -161,24 +165,59 @@ def report_mean_ci(dfname: str):
     
 def test_scores(Y_true, Y_pred):
     accuracy, sens, spec, ppv, npv, auc = conmatscores(Y_true, Y_pred)
-    print ("accuracy", accuracy*100, "sens", sens*100, "spec", spec*100, "ppv", ppv*100, "npv", npv*100, "auc", auc)
+    print ("accuracy: ", round(accuracy*100,2))
+    print ("sensitivity: ", round(sens*100,2))
+    print ("specificity: ", round(spec*100,2))
+    print ("positive predictive value: ", round(ppv*100,2))
+    print ("negative predictive value: ", round(npv*100,2))
+    print ("Area-under-the-curve:", round(auc,3))
 
+def write_report(metric_type: str, Y_true, Y_pred):
+    if metric_type == 'train':
+        logger.info("--TRAINING METRICS--")
+    elif metric_type == 'test':
+        logger.info("--TESTING METRICS--")
+    accuracy, sens, spec, ppv, npv, auc = conmatscores(Y_true, Y_pred)
+    auc_score = ("AUC: " + str(round(auc,3)))
+    sens_score = ("sensitivity: " + str(round(sens*100,2)))
+    spec_score = ("specificity: " + str(round(spec*100,2)))
+    ppv_score = ("positive predictive value: " + str(round(ppv*100,2)))
+    npv_score = ("negative predictive value: " + str(round(npv*100,2)))
+    acc_score = ("accuracy: " + str(round(accuracy*100,2)))
+    logger.info(auc_score)
+    logger.info(sens_score)
+    logger.info(spec_score)
+    logger.info(ppv_score)
+    logger.info(npv_score)
+    logger.info(acc_score)
 
-#================== AD vs DLB ==================
-advdlb=pickle.load(open(directories.infile_dir + 'ad_v_dlb.pkl', 'rb'))
+#================== calculate metrics ==================
+logger.info("calulating metrics")
+#AD vs DLB
+logger.info("AD vs DLB")
+#advdlb=pickle.load(open(directories.infile_dir + 'ad_v_dlb.pkl', 'rb'))
 #train
 df_tr_trim_addlb = df_tr_trim.loc[(df_tr_trim.GroupID!=3) & (df_tr_trim.GroupID!=4)]
 df_tr_trim_addlb_redef = df_tr_trim_addlb.replace(dict_list[1])
 Y_true_tr_addlb = np.array(df_tr_trim_addlb_redef.GroupID)
 Y_pred_tr_addlb = np.array(df_tr_trim_addlb_redef["dmri_ad_v_dlb (AD Probability)"])
+bootstrapper(Y_true_tr_addlb, Y_pred_tr_addlb, savename="ad_v_dlb_train")
+report_mean_ci(dfname="ad_v_dlb_train")
+test_scores(Y_true_tr_addlb, Y_pred_tr_addlb)
+write_report('train', Y_true_tr_addlb, Y_pred_tr_addlb)
+
 #test
 df_te_trim_addlb = df_te_trim.loc[(df_te_trim.GroupID!=3) & (df_te_trim.GroupID!=4)]
 df_te_trim_addlb_redef = df_te_trim_addlb.replace(dict_list[1])
 Y_true_te_addlb = np.array(df_te_trim_addlb_redef.GroupID)
 Y_pred_te_addlb = np.array(df_te_trim_addlb_redef["dmri_ad_v_dlb (AD Probability)"])
-
-bootstrapper(Y_true_tr_addlb, Y_pred_tr_addlb, savename="ad_v_dlb_train")
 bootstrapper(Y_true_te_addlb, Y_pred_te_addlb, savename="ad_v_dlb_test")
-report_mean_ci(dfname="ad_v_dlb_train")
 report_mean_ci(dfname="ad_v_dlb_test")
 test_scores(Y_true_te_addlb, Y_pred_te_addlb)
+write_report('test', Y_true_te_addlb, Y_pred_te_addlb)
+
+
+
+
+# exit sys
+sys.exit()
