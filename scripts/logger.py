1d36c533740513754c93dc43952d9429301ca3ff
jessedesimone <52834672+jessedesimone@users.noreply.github.com>
2023-10-27 14:47:10 -0400

    231027

---
+++ scripts/logger.py
@@ -0,0 +1,18 @@
+import logging
+from  datetime import datetime
+def configure_logger(path):
+    """ Configures logger used throughout the application
+        To get an instance of the configured logger from any module, simply call:
+            `logger = logging.getLogger(__name__)`
+    """
+
+    dt_string = datetime.now().strftime("%Y_%m_%d_%H_%M_%S_%p")
+
+    logging.basicConfig(
+        level=logging.INFO,
+        format="%(asctime)s [%(threadName)-12.12s] [%(levelname)-5.5s]  %(message)s",
+        handlers=[
+            logging.FileHandler(filename = path + f"out_{dt_string}.log"),
+            logging.StreamHandler()
+        ]
+    )
